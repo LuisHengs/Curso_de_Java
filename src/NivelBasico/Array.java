344f8682b6bd1e2e82d7387631cb8b2c601a9fe1
luishengs <luis.hengs@protonmail.com>
2025-09-16 22:39:17 -0300

    Aula sobre Array

---
+++ src/NivelBasico/Array.java
@@ -0,0 +1,17 @@
+package NivelBasico;
+
+public class Array {
+    public static void main(String[] args) {
+
+        String[] ninja = new String[3];
+        ninja[0] = "Naruto Uzumaki";
+        ninja[1] = "Sasuke Uchiha";
+        ninja[2] = "Sakura Haruno";
+
+        System.out.println(ninja[2]);
+
+        // int[] idade = new int[3];
+
+
+    }
+}
