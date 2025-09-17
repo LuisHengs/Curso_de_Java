cccbfee085fb81c063c5ff567c3304a3c8fa7ef7
luishengs <luis.hengs@protonmail.com>
2025-09-16 22:05:58 -0300

    Aula sobre Ternarios

---
+++ src/Condicoes/Ternarios.java
@@ -0,0 +1,16 @@
+package Condicoes;
+
+public class Ternarios {
+    public static void main(String[] args) {
+
+        /*
+        * Ternarios: São maneiras de reduzir o código
+        * variavel = (condição) ? valorSeVerdadeiro : valorSeFalso;
+        * */
+
+        short numeroDeMissoes = 11;
+        String nivel = (numeroDeMissoes >= 10) ? "Esse ninja esta com mais de 10 missoes" : "Esse ninja tem menos de 10 missoes";
+        System.out.println(nivel);
+
+    }
+}
