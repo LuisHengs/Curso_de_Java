8019c997a3c37f7721d2dfc8270b703735f56f8e
luishengs <luis.hengs@protonmail.com>
2025-09-20 02:34:13 -0300

    Aula sobre como aplicar metodos aos objetos

--- src/NivelIntermediario/Main.java
+++ src/NivelIntermediario/Main.java
@@ -15,6 +15,14 @@ public class Main {
         Sasuke.aldeia = "Aldeia da Folha";
         Sasuke.idade = 18;
 
+        // Aplicando Metodos aos meus objetos
+        Sasuke.SharinganAtivado();
+        String chamandoMetodo = Sasuke.EuSouUmNinja();
+        System.out.println(chamandoMetodo);
+
+        int quantoTempoFalta = Sasuke.anosParaSeTornarHokage(70);
+        System.out.println("Você tem: " + Sasuke.idade + " então falta no minimo: " + quantoTempoFalta + " anos para você se tornar Hokage");
+
         // Criar ninja Sakura Haruno - Haruno é um OBJETO
         Ninja Sakura = new Ninja();
         Sakura.nome = "Sakura Haruno";
--- src/NivelIntermediario/Ninja.java
+++ src/NivelIntermediario/Ninja.java
@@ -6,4 +6,27 @@ public class Ninja {
     String aldeia;
     int idade;
 
+    /*
+    * O metodo VOID não retorna valor nenhum!
+    * */
+
+    // Criar um metodo Publico personalizado
+    public void SharinganAtivado() {
+        System.out.println("O sharigan Ativou");
+    }
+
+    /*
+    * Metodo String vai ter que retornar uma String
+    * */
+    public String EuSouUmNinja() {
+        return "Oi, Eu sou um ninja!";
+    }
+
+    /*
+    * Metodo Int vai ter que retornar um int
+    * */
+    public int anosParaSeTornarHokage(int idadeMinimaParaSerHokage) {
+        return idadeMinimaParaSerHokage - idade;
+    }
+
 }
