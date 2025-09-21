a5887120055cb842d9ddf2fefd9d29025b127ce0
luishengs <luis.hengs@protonmail.com>
2025-09-20 23:50:06 -0300

    Desafio sobre orientação a objeto

---
+++ src/Desafio3/Ninja.java
@@ -0,0 +1,19 @@
+package Desafio3;
+
+public class Ninja {
+    String nome;
+    int idade;
+    String missao;
+    String nivelDeDificuldade;
+    String statusDaMissao;
+
+    public void mostrarInformacoes(){
+        System.out.println("nome = " + nome);
+        System.out.println("Nivel de dificuldade = " + nivelDeDificuldade);
+        System.out.println("Status da Missão = " + statusDaMissao);
+        System.out.println("missão = " + missao);
+        System.out.println("idade = " + idade);
+
+    }
+
+}
---
+++ src/Desafio3/Uchiha.java
@@ -0,0 +1,21 @@
+package Desafio3;
+
+public class Uchiha extends Ninja {
+
+    String habilidadeEspecial;
+
+    public void mostrarHabilidade() {
+        System.out.println("Habilidade Especial: " + habilidadeEspecial);
+    }
+
+    public void mostrarInformacoes(){
+            System.out.println("nome = " + nome);
+            System.out.println("idade = " + idade);
+            System.out.println("missão = " + missao);
+            System.out.println("Nivel de dificuldade = " + nivelDeDificuldade);
+            System.out.println("Status da Missão = " + statusDaMissao);
+            System.out.println("habilidade especial Uchiha ");
+
+    }
+
+}
