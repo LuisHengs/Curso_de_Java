1e6df6d4ea6098a65861d94341d421c9e4c47dd0
luishengs <luis.hengs@protonmail.com>
2025-09-24 22:35:12 -0300

    Construtores

---
+++ src/Construtores/Hokages.java
@@ -0,0 +1,43 @@
+package Construtores;
+
+public class Hokages {
+
+    String nome;
+    int idade;
+    boolean vivoOuNao;
+    String aldeia;
+    int missoes;
+    double saldoBancario;
+    double altura;
+
+    public Hokages() {
+        // Construtor vazio, sem argumentos
+    }
+
+    // Criar um construtor com argumento
+    public Hokages(String nome) {
+        this.nome = nome;
+    }
+
+    public Hokages(int idade){
+        this.idade = idade;
+    }
+
+    // All args constructor = Construtor com todos os argumentos
+    public Hokages(String nome, int idade, boolean vivoOuNao){
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoOuNao = vivoOuNao;
+    }
+
+    // Você consegue criar altomaticamente os construtores com intellij (Alt + Insert)
+    public Hokages(String nome, int idade, boolean vivoOuNao, String aldeia, int missoes, double saldoBancario, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoOuNao = vivoOuNao;
+        this.aldeia = aldeia;
+        this.missoes = missoes;
+        this.saldoBancario = saldoBancario;
+        this.altura = altura;
+    }
+}
---
+++ src/Construtores/Main.java
@@ -0,0 +1,18 @@
+package Construtores;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Hokages Hashirama = new Hokages();
+        Hashirama.nome = "Hashirama Senju";
+
+        Hokages Tobirama = new Hokages("Tobirama Senju");
+        System.out.println(Tobirama.nome);
+
+        Hokages Hiruzen = new Hokages(40);
+        System.out.println(Hiruzen.idade);
+
+        Hokages Minato = new Hokages("Minato Namikaze", 32, false);
+        System.out.println(Minato.idade);
+    }
+}
