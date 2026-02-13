bce901b7d407e93b53ffb803dc85ca0e615a7a5f
luishengs <luis.hengs@protonmail.com>
2026-02-13 20:47:44 -0300

    linkedlist uma explicação de collections

---
+++ src/NivelIntermediario/listasEncadeadas.java
@@ -0,0 +1,39 @@
+package NivelIntermediario;
+
+import java.util.*;
+
+public class listasEncadeadas {
+    public static void main(String[] args) {
+
+
+        // Classes
+        ArrayList<String> arrayList = new ArrayList<>();
+        LinkedList<String> linkedList = new LinkedList<>();
+        Stack<String> stack = new Stack<>();
+
+        Queue<String> queue = new LinkedList<>();
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>();
+
+        linkedList.add("Naruto");
+        System.out.println(linkedList);
+
+
+
+
+
+
+        /* LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("Naruto Uzumaki");
+        linkedList.add("Sasuke Uchiha");
+        linkedList.add("Sakura Haruno");
+
+        System.out.println(linkedList);
+
+        linkedList.add(1, "Kakashi Hatake");
+        System.out.println(linkedList);
+
+        linkedList.remove(2);
+        System.out.println(linkedList); */
+
+    }
+}
