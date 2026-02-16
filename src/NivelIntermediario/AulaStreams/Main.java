1cbaf7dc4ad177ca9758c977864d178a0f1e4ab5
luishengs <luis.hengs@protonmail.com>
2026-02-16 20:59:43 -0300

    Streams como economizar código(Parte 2)

--- src/NivelIntermediario/AulaStreams/Main.java
+++ src/NivelIntermediario/AulaStreams/Main.java
@@ -17,17 +17,34 @@ public class Main {
 
 
         // .stream
-        // Filtragem dos ninjas por vila
+        // FILTER Filtragem dos ninjas por vila
         /*ninjas.stream()
                 .filter(ninja -> ninja.getVila().equals("Konoha"))
                 .forEach(System.out::println);
         ;*/
 
-        // Ordenação
-        ninjas.stream()
-                .sorted((n1, n2) -> Integer.compare(n1.getIdade(), n2.getIdade()))
+        // SORTED Ordenação
+        /* ninjas.stream()
+                .sorted((n1, n2) -> n1.getNome().compareTo(n2.getNome()))
                 .forEach(System.out::println);
-        ;
+        ; */
+
+        // MAP - Mostrar e mapear um atributo
+        /* ninjas.stream()
+                .map(Ninja::getNome)
+                .forEach(System.out::println);
+
+        ninjas.stream()
+                .map(Ninja::getIdade)
+                .forEach(System.out::println);
+        */
+
+        // MAX - Filtrar por ninja mais velho
+        Ninja ninjaMaisVelho = ninjas.stream()
+                .max((n1, n2) -> Integer.compare(n1.getIdade(), n2.getIdade()))
+                .orElse(null);
+
+        System.out.println("Ninja filtrado pelo mais velho = " + ninjaMaisVelho);
 
 
     }
